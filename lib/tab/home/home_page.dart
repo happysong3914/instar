effd616820e1116fc03493415058a8ae8580aa5a
jin song <songjin3914@gmail.com>
2024-04-17 17:57:07 +0900

    home화면 완성

--- lib/tab/home/home_page.dart
+++ lib/tab/home/home_page.dart
@@ -11,15 +11,101 @@ class HomePage extends StatelessWidget {
           'Instagram Clone',
         ),
       ),
-      body: Column(
-        children: [
-          Text(
-            'Instagram에 오신 것을 환영합니다',
-          ),
-          Text(
-            'Instagram에 오신 것을 환영합니다',
-          ),
-        ],
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Instagram에 오신 것을 환영합니다',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              '사진과 동영상을 보려면 팔로우하세요',
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Card(
+              elevation: 4.0,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: 80,
+                      height: 80,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            'https://i.namu.wiki/i/KHZxgx6dilwr4Z7uu6wSPoVlf5aIb6rq6qIOBV2LYBYdN9cWFaLlvkggojNNTD6mrwtGxS_lTPh4Woge2hzuZQ.webp'),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'test@test.com',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text('닉네임'),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.network(
+                          'https://i.namu.wiki/i/KHZxgx6dilwr4Z7uu6'
+                          'wSPoVlf5aIb6rq6qIOBV2LYBYdN9cWFaLlvkggojNNTD6mrwtGxS_lTPh4Woge2hzuZQ.webp',
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Image.network(
+                          'https://i.namu.wiki/i/KHZxgx6dilwr4Z7uu6'
+                              'wSPoVlf5aIb6rq6qIOBV2LYBYdN9cWFaLlvkggojNNTD6mrwtGxS_lTPh4Woge2hzuZQ.webp',
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Image.network(
+                          'https://i.namu.wiki/i/KHZxgx6dilwr4Z7uu6'
+                              'wSPoVlf5aIb6rq6qIOBV2LYBYdN9cWFaLlvkggojNNTD6mrwtGxS_lTPh4Woge2hzuZQ.webp',
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+
+                      ],
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text('Facebook친구'),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('팔로우'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
